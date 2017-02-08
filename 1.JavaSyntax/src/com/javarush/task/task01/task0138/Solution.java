0845e84e3f40a938a44da525875084086d73107c
determinant1987 <moiseev.ism@gmail.com>
2017-02-09 01:22:21 +0300

    add task0138

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0138/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task01.task0138;
+
+/* 
+Любимое стихотворение
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Мое любимое стихотворение:");
+        //напишите тут ваш код
+        System.out.println("Бывает ли женщина в жизни хоть раз неправа?");
+        System.out.println("Безумству вопроса нам следует лишь подивиться.");
+        System.out.println("Спросивший такое не просто болван-голова,");
+        System.out.println("Но хуже гораздо: практически самоубийца!");
+    }
+}
