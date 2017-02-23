ef3b08bd0fa0002fbc5aa0b99b474425bb742e7d
determinant1987 <moiseev.ism@gmail.com>
2017-02-24 01:39:36 +0300

    add tasks  0901 - 0906

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0902/Solution.java
@@ -0,0 +1,58 @@
+package com.javarush.task.task09.task0902;
+
+/* 
+И снова StackTrace
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        method1();
+    }
+
+    public static String method1() {
+        method2();
+        //напишите тут ваш код
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        String x = stackTraceElements[2].getMethodName();
+
+        return x;
+    }
+
+    public static String method2() {
+        method3();
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        String x = stackTraceElements[2].getMethodName();
+
+        //напишите тут ваш код
+        return x;
+    }
+
+    public static String method3() {
+        method4();
+
+        //напишите тут ваш код
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        String x = stackTraceElements[2].getMethodName();
+
+        return x;
+    }
+
+    public static String method4() {
+        method5();
+        //напишите тут ваш код
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        String x = stackTraceElements[2].getMethodName();
+
+        return x;
+    }
+
+    public static String method5() {
+        //напишите тут ваш код
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        String x = stackTraceElements[2].getMethodName();
+
+
+        return x;
+
+    }
+}
