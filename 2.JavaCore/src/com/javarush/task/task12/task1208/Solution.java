c3af4a9e58d179998b2a37dcac90a087e1628ec2
determinant1987 <moiseev.ism@gmail.com>
2017-03-19 14:32:06 +0300

    add task1206-1210

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Пять методов print с разными параметрами
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    public static void print(int a){}
+    public static void print(int a, int b){}
+    public static void print(String a){}
+    public static void print(String a, String b){}
+    public static void print(float a){}
+
+
+}
